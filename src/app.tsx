39c2839ea579aa943a6cc5436db790a5b9da3c07
Alex Jaden <aksairensan@gmail.com>
2026-06-10 16:09:48 +0500

    Format app.tsx (editor auto-format)

--- src/app.tsx
+++ src/app.tsx
@@ -20,7 +20,9 @@ export default function App() {
       <Router
         root={(props) => (
           <MetaProvider>
-            <SolidBaseRoot meta={{ provider: false }}>{props.children}</SolidBaseRoot>
+            <SolidBaseRoot meta={{ provider: false }}>
+              {props.children}
+            </SolidBaseRoot>
           </MetaProvider>
         )}
       >
